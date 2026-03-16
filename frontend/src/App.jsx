0e40c4fd42c4dc45b393803be191e1b91f5d8511
Karolis Tarleckas <karolis.tarleckas@stud.vilniustech.lt>
2026-03-16 13:23:47 +0200

    SCRUM-53 UR8.2 - Develop 'My Favorites' List View Page (GUI)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import QuestionnairePage from './pages/QuestionnairePage';
 import AnimalListPage from "./pages/AnimalListPage/AnimalListPage.jsx";
 import MainAnimalsPage from './pages/MainAnimalsPage';
+import FavoritesPage from './pages/FavoritesPage.jsx';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Link to="/" style={{ marginRight: '40px', color: '#333' }}>Pradžia</Link>
             <Link to="/mainAnimals" style={{ marginRight: '40px', color: '#333' }}>Gyvūnai</Link>
             <Link to="/animals" style={{ marginRight: '40px', color: '#333' }}>Savanoriams (Gyvūnai)</Link>
+            <Link to="/favorites" style={{marginRight: '40px', color: '#333'}}>Mano mėgstamiausi</Link>
             <Link to="/questionnaire" style={{marginRight: '40px', color: '#333'}}>Klausimynas</Link>
         </nav>
         <Routes>
@@ -20,6 +22,7 @@ function App() {
           <Route path="/questionnaire" element={<QuestionnairePage />} />
           <Route path="/animals" element={<AnimalListPage />} />
           <Route path="/mainAnimals" element={<MainAnimalsPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
         </Routes>
       </div>
   );
