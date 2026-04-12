92e3b2d24499332abd17cee83ca7895eb1bc23bd
matiusovid92 <matiusovid92@gmail.com>
2026-04-12 21:45:20 +0300

    Pakeitimai:
    Pakeistas navbar, kuris keicia turini priklausomai nuo roles
    backend prie login grazinamos info prideta role
    index css pakeistas, kad visada butu light tema

--- backend/src/Modules/Users/Application/userService.js
+++ backend/src/Modules/Users/Application/userService.js
@@ -96,7 +96,8 @@ const login = async (email, password) => {
             id: user.id,
             name: user.name,
             email: user.email,
-            points: user.points
+            points: user.points,
+            role: user.role
         }
     };
 }
